8a731696201f205de28e87afdef29cd9dfef5565
Samtfm <sfmemail@gmail.com>
2020-08-01 14:06:47 -0500

    update config

--- src/index.js
+++ src/index.js
@@ -20,30 +20,10 @@ import Login from 'screens/Login'
 import ViewRock from 'screens/ViewRock'
 import AuthLoaded from 'components/AuthLoaded.component'
 
-
-// https://rnfirebase.io/reference/app/firebaseappoptions
-const fbConfig = {
-  androidClientId: undefined, // iOS only
-  apiKey: "AIzaSyCyjvCW1nT4qQszuOf0N42Umw3wMWh6SYQ",
-  appId: "1:342705485686:android:98b6ee6c26ed1d04deac57",
-  clientId: undefined, // iOS only
-  databaseURL: "https://nice-rocks.firebaseio.com",
-  deepLinkURLScheme: undefined, // iOS only
-  gaTrackingId: undefined,
-  messagingSenderId: "342705485686",
-  projectId: "nice-rocks",
-  storageBucket: "nice-rocks.appspot.com",
-}
-
 // react-redux-firebase config
 const rrfConfig = {
  userProfile: 'users',
  useFirestoreForProfile: true,// Firestore for Profile instead of Realtime DB
-}
-
-// Initialize firebase instance
-if (RNFirebase.apps.length == 0){
-  RNFirebase.initializeApp(fbConfig)
 }
 
 // Initialize other services on firebase instance
