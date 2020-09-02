db8e4c74e1bdfad3eb41ed45989fa04053f648a9
Samtfm <sfmemail@gmail.com>
2020-09-02 14:22:03 -0500

    bump version and update FAB

--- src/components/ComposeButton.tsx
+++ src/components/ComposeButton.tsx
@@ -2,50 +2,72 @@ import React from 'react';
 import { StyleSheet, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import colors from 'styles/colors';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { FAB } from 'react-native-paper';
 
 const ComposeButton = () => {
   const navigation = useNavigation();
 
-  return (
-    <Pressable
-      onPress={() => navigation.navigate('ComposeRock')}
-      style={({ pressed }) => [
-        {
-          backgroundColor: pressed
-            ? '#61CAF6'
-            : colors.blue,
-        },
-        styles.composeButton
-      ]}>
-      <Icon name={'plus'} color={'white'} size={24} />
-    </Pressable>
-  );
-}
+  return <FAB
+    style={styles.fab}
+    // small
+    color={colors.white}
+    icon="plus"
+    onPress={() => navigation.navigate('ComposeRock')}
+  />
+};
 
 const styles = StyleSheet.create({
-  composeButton: {
-    borderRadius: 100,
-    width: 50,
-    height: 50,
-    position:'absolute',
-    bottom: 40,
+  fab: {
+    position: 'absolute',
+    backgroundColor: colors.blue,
+    margin: 16,
     right: 0,
-    margin: 20,
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    bottom: 60,
+},
+})
 
-    //android
-    elevation: 10,
+// const ComposeButton = () => {
+//   const navigation = useNavigation();
 
-    //ios
-    shadowColor: 'black',
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-  },
-});
+//   return (
+//     <Pressable
+//       onPress={() => navigation.navigate('ComposeRock')}
+//       style={({ pressed }) => [
+//         {
+//           backgroundColor: pressed
+//             ? '#61CAF6'
+//             : colors.blue,
+//         },
+//         styles.composeButton
+//       ]}>
+//       <Icon name={'plus'} color={'white'} size={24} />
+//     </Pressable>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   composeButton: {
+//     borderRadius: 100,
+//     width: 50,
+//     height: 50,
+//     position:'absolute',
+//     bottom: 40,
+//     right: 0,
+//     margin: 20,
+//     flex: 1,
+//     flexDirection: 'column',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+
+//     //android
+//     elevation: 10,
+
+//     //ios
+//     shadowColor: 'black',
+//     shadowOffset: { width: 0, height: 4 },
+//     shadowOpacity: 0.3,
+//     shadowRadius: 4,
+//   },
+// });
 
 export default ComposeButton;
