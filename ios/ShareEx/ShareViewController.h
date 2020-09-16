396151c45a5f810fdba7757b3d3b5ae23ffb580d
Samtfm <sfmemail@gmail.com>
2020-09-16 17:21:24 -0400

    WIP add ios share extension

---
+++ ios/ShareEx/ShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.h
+//  ShareEx
+//
+//  Created by Sam Faber-Manning on 9/16/20.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
