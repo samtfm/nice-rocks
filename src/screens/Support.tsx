2156e84d03e5639538631bd981d808e904092709
Samtfm <sfmemail@gmail.com>
2020-12-10 02:38:06 -0800

    add info to support screen

--- src/screens/Support.tsx
+++ src/screens/Support.tsx
@@ -1,14 +1,40 @@
 import Text from "components/Text";
 import React, { ReactElement } from "react";
-import { View } from "react-native";
+import { View, StyleSheet, Pressable, Linking } from "react-native";
+import colors from "styles/colors";
 
+const supportEmail = "nicerocks@googlegroups.com"
 const Support = (): ReactElement => {
-
   return (
-    <View>
-      <Text>Support info</Text>
+    <View style={styles.main}>
+      <Text style={styles.infoText}>{
+      "Please send questions, bugs, or report abuse to"
+      }
+      <Pressable
+        onPress={() => Linking.openURL(`mailto:${supportEmail}`)}
+        >
+        <Text style={styles.supportEmail}
+        >{supportEmail}</Text>
+      </Pressable>
+
+      </Text>
     </View>
   )
 };
-
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  infoText: {
+    margin: 'auto',
+    textAlign: 'center',
+  },
+  supportEmail: {
+    color: colors.blue,
+    marginTop: 10,
+    textAlign: 'center',
+  },
+})
 export default Support;
+
